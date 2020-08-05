d3ad5af35201a71aff87ad01a5eb735fb3365560
Tahj Anderson <tahjanderson@Tahjs-MacBook-Pro.local>
2020-08-05 15:40:53 -0400

    Changes in the about page and home page visually

--- src/Components/FeaturedPosts.js
+++ src/Components/FeaturedPosts.js
@@ -6,7 +6,7 @@ export default class FeaturedPosts extends Component{
     render(){
         return(
             <div>
-                <h1>Featured Posts</h1>
+                <h1 className="H1-style">Featured Posts</h1>
                 <div className="Featured-Posts" >
                     <PostCard />
                     <PostCard />
